aac56dd79259af367d9855eb26f2fb0488f8fec0
Gitlalala1 <gitbumlala@mail.ru>
2023-05-10 16:13:37 +0300

    User hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const initialState = {
+	name: "Bomik",
+	age: 30,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [count, multiply, share] = useCounter(1, 3);
 
+	const [obj, setObj] = useMergeState(initialState);
+	console.log(obj);
+	return (
+		<div className="App">
+			<div className="counter">
+				<button onClick={multiply}>click *</button>
+				<button onClick={share}>click /</button>
+				<span>{count}</span>
+			</div>
+			<div className="mergeState">
+				<div>
+					<input
+						type="text"
+						onChange={(e) => setObj({ text: e.target.value })}
+					/>
+					<div>
+						<State obj={obj} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+const State = ({ obj }) => {
+	return (
+		<div style={{ display: "flex" }}>
+			<div>
+				{Object.keys(obj).map((el) => (
+					<span style={{ display: "block" }}>{el}</span>
+				))}
+			</div>
+			<div>
+				{Object.values(obj).map((el) => (
+					<span style={{ display: "block" }}>{el}</span>
+				))}
+			</div>
+		</div>
+	);
+};
+const useMergeState = (initialState) => {
+	const [state, setState] = useState(initialState);
+
+	const mergeState = (changes) => {
+		setState((prev) => ({
+			...prev,
+			...changes,
+		}));
+	};
+
+	return [state, mergeState];
+};
+
+//
+const useCounter = (initValue = 1, delta = 2) => {
+	const [state, setState] = useState(initValue);
+
+	const multiply = () => {
+		setState((state) => state * delta);
+	};
+
+	const share = () => {
+		setState((state) => state / delta);
+	};
+
+	return [state, multiply, share];
+};
 export default App;
