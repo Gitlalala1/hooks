ed65d35593e4d85aa0a4774d1f00fc4615b96f39
Gitlalala1 <gitbumlala@mail.ru>
2023-05-12 09:34:55 +0300

    useRef. Не рендерит компоненту

--- src/App.js
+++ src/App.js
@@ -1,80 +1,40 @@
-import React, { useState, useEffect, useLayoutEffect, useMemo } from "react";
+import React, {
+	useState,
+	useEffect,
+	useLayoutEffect,
+	useRef,
+	useMemo,
+} from "react";
 
-const useCharacterPosition = (step) => {
-	const [left, setLeft] = useState(0);
-	const [top, setTop] = useState(0);
-
-	useEffect(() => {
-		const handleKeyDown = (event) => {
-			switch (event.key) {
-				case "ArrowLeft":
-					setLeft((prev) => prev - step);
-					break;
-				case "ArrowRight":
-					setLeft((prev) => prev + step);
-					break;
-				case "ArrowUp":
-					setTop((prev) => prev - step);
-					break;
-				case "ArrowDown":
-					setTop((prev) => prev + step);
-					break;
-			}
-		};
-
-		document.addEventListener("keydown", handleKeyDown);
-		return () => {
-			document.removeEventListener("keydown", handleKeyDown);
-		};
-	}, [step]);
-
-	return [left, top];
-};
-const initialStyle = {
-	background: "blue",
-	width: 100,
-	height: 100,
-	position: "absolute",
-	left: 0,
-	top: 0,
-	transition: ".1s",
-};
 const App = () => {
-	const [left, top] = useCharacterPosition(50);
-	//const [style, setStyle] = useState(initialStyle);
-
-	const style = useMemo(() => {
-		return {
-			...initialStyle,
-			left,
-			top,
-		};
-	}, [left, top]);
-
-	// useLayoutEffect(() => {
-	// 	setStyle((prev) => ({
-	// 		...prev,
-	// 		top,
-	// 		left,
-	// 	}));
-	// }, [left, top]);
-
-	const generateBlocks = (count) => {
-		const divs = [];
-		for (let i = 0; i < count; i++) {
-			divs.push(<div>i={i}</div>);
-		}
-		return divs;
+	const inputRef = useRef();
+	const toggleRef = useRef(false);
+	const [toogleState, setToggleState] = useState(false);
+	const onFocus = () => {
+		inputRef.current.focus();
 	};
-
+	const onChange = (e) => {
+		if (e.target.value === "blur") inputRef.current?.blur();
+	};
+	const changeToggleRef = () => {
+		toggleRef.current = !toggleRef.current;
+		console.log("toggle ref");
+	};
+	const changeToggleState = () => {
+		setToggleState((prev) => !prev);
+		console.log("toggle state");
+	};
+	console.log("render");
 	return (
 		<div>
-			<h3>
-				[{left},{top}]
-			</h3>
-			<div style={style}></div>
-			{generateBlocks(15000)}
+			<input type="text" ref={inputRef} onChange={onChange} />
+			<button onClick={onFocus}>Focus</button>
+			<div>
+				<button onClick={changeToggleRef}>toggle ref</button>
+				<button onClick={changeToggleState}>toggle state</button>
+			</div>
 		</div>
 	);
 };
+
 export default App;
